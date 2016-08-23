07eee761a70a55c110cecd82c1515f91f8cd09f2
David Jackson <jack8679@kettering.edu>
2016-08-23 16:30:23 -0400

    The Office Text Analysis - quotes.json and load.js
    quotes.json contains all of the quotes from the show, yet to be
    separated into individual lines.
    
    load.js is a result of migrating over the XMLHttpRequest code into an
    individual file, which could be useful for future projects. It can now
    return HTML, XML, and JSON asynchronously.

--- The-Office-Text-Analysis/script.js
+++ The-Office-Text-Analysis/script.js
@@ -1,4 +1,27 @@
 
+
+var text = "<b>Pam:</b> Hey.<b>Jim:</b> Hey.<b>Jim:</b> How are things?<b>Pam:</b> Good. I thought you were going out for a drink with...<b>Jim:</b> Oh no, I just decided not to. How's your headache?<b>Pam:</b> It's better, thanks.<b>Jim:</b> Good. Good.<b>Pam:</b> Yeah.<b>Jim:</b> That's great<b>Pam:</b> Is...?<b>Jim:</b> Yeah?<b>Pam:</b> Um... Are you...<b>Jim:</b> Am I walking out?<b>Pam:</b> Yes.<b>Jim:</b> Yes, I... Do you want to...<b>Pam:</b> Yeah.<b>Jim:</b> Great. Let me just...<b>Jim:</b> [Car horn honking] Oh, Roy.<b>Pam:</b> Yeah. Listen, have a nice weekend.<b>Jim:</b> Yeah, definitely. You too. Enjoy it. [looks at camera] You know what, just come here.";
+
+var arr = text.split("<b>");
+arr.splice(0,1);
+
+for (var i = 0; i < arr.length; i++) {
+  var a = arr[i].split(":</b>");
+  arr[i] = {
+    "saidBy": a[0],
+    "line": a[1].trim()
+  };
+}
+
+console.log(arr);
+
+
+
+
+
+
+
+ /* Beginning of Loading Episodes */
 var episodes = [];
 var episodeIndex = 0;
 
@@ -36,10 +59,21 @@ function loadEpisode() {
 
 function parseEpisode(html) {
   var quotes = html.getElementsByClassName("quote");
-  episodes[episodeIndex].quotes = quotes;
+  //episodes[episodeIndex].quotes = [];
+  episodes[episodeIndex].lines = [];
+  for (var i = 0; i < quotes.length; i++) {
+    var quote = quotes[i].innerHTML.trim().replaceAll("<br>", "");
+    var qArr = quote.split("\n");
+    for (var j = 0; j < qArr.length; j++) {
+      qArr[j] = qArr[j].trim();
+    }
+    quote = qArr.join("");
+    episodes[episodeIndex].quotes.push(quote);
+  }
+
   document.getElementById("epNum").innerHTML = episodeIndex;
   episodeIndex++;
-  if (false) {//episodeIndex < episodes.length) {
+  if (episodeIndex < episodes.length) {
     load(episodes[episodeIndex].link, parseEpisode);
   } else {
     document.getElementById("epNum").innerHTML = episodeIndex + " - done.";
@@ -61,22 +95,10 @@ function parseEpisodeNum(link) {
 
 
 
-// load any url with a callback that returns an html element
-function load(theURL, callback) {
-  var xhttp;
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      var xmlDoc = document.createElement("html");
-      xmlDoc.innerHTML = xhttp.responseText;
-      callback(xmlDoc);
-    }
-  }
-  xhttp.open("GET", "http://mighty-falls-90233.herokuapp.com/get/?url=" + theURL, true);
-  xhttp.send();
-}
+
 
 String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
+ /* End of Loading Episodes */
